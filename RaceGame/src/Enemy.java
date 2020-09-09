1a11805d12d5f21a3611ceb989a5a8e4c5ec6e57
TernyFolls <zadtol@mail.ru>
2020-09-10 01:45:40 +0300

    Race v0.2

--- RaceGame/src/Enemy.java
+++ RaceGame/src/Enemy.java
@@ -1,24 +1,50 @@
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.util.Random;
+
 import javax.swing.ImageIcon;
 
 public class Enemy {
 	double x;
 	double y;
 	double v;
+	int diff;
+	boolean direction;
+
 	Image img = new ImageIcon("res/enemy.png").getImage();
 	Road road;
-	
-	public Enemy(int x, int y, int v, Road road) {
+
+	public Enemy(int x, int y, int v, int diff, Road road) {
 		this.x = x;
 		this.y = y;
 		this.v = v;
 		this.road = road;
-				
+		this.diff = diff;
+		direction = new Random().nextBoolean();
+	}
+
+	public Rectangle hitbox() {
+		return new Rectangle((int) x + img.getWidth(null) / 4, (int) y + img.getHeight(null) / 4,
+				img.getWidth(null) / 2, img.getHeight(null) / 2);
+		
+	}
+	
+	public double getX() {
+		return x;
 	}
 	
 	public void move() {
 		x = x - road.p.v + v;
+		if (diff == 1) {
+			if (y < Player.MAX_TOP) {
+				direction = false;
+			}
+			if (y > Player.MAX_BOT) {
+				direction = true;
+			}
+
+			y += (direction ? -1 : 1) * 6;
+		}
 	}
-	
-	
+
 }
--- RaceGame/src/Player.java
+++ RaceGame/src/Player.java
@@ -1,4 +1,5 @@
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
@@ -6,26 +7,33 @@ import javax.swing.JOptionPane;
 
 public class Player {
 
+	
 	public static final double MAX_V = 80;
 	public static final double MAX_TOP = 200;
 	public static final double MAX_BOT = 600;
 	public static final double SHAKE = 5;
 	public static final double COFF = 3;
-	
+
 	Image img = new ImageIcon("res/player.png").getImage();
 
-	
 	boolean sw = false;
 	double v = 0;
 	double dv = 0;
 	double dy = 0;
 	double s = 0;
 
+	
 	double x = 30;
 	double y = 300;
 
 	double layer1 = 0;
 	double layer2 = 1514;
+
+	public Rectangle hitbox() {
+		return new Rectangle((int) x + img.getWidth(null) / 4, (int) y + img.getHeight(null) / 4,
+				img.getWidth(null) / 2, img.getHeight(null) / 2);
+		
+	}
 
 	public void move() {
 		s += v;
@@ -35,16 +43,15 @@ public class Player {
 		if (v >= MAX_V) {
 			v -= 1;
 		}
-		
-		if (v >= MAX_V - 40 ) {
-			y -= 0.1 * Math.pow(2, 0.1*v);
+
+		if (v >= MAX_V - 40) {
+			y -= 0.1 * Math.pow(2, 0.1 * v);
 			if (sw) {
-				y += 0.2 * Math.pow(2, 0.1*v);
+				y += 0.2 * Math.pow(2, 0.1 * v);
 			}
 			sw = !sw;
 		}
-		
-		
+
 		y -= dy;
 		if (layer2 - v <= 0) {
 			layer1 = 0;
@@ -58,7 +65,7 @@ public class Player {
 			y -= 10;
 			dy = 0;
 		}
-		
+
 		layer1 -= v;
 		layer2 -= v;
 	}
